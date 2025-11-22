0aeb3143a80ee0dd8a3abe6b3d2ebd86b2a29eed
Victor Humberto Arias Caballero <uti76@atu.gob.pe>
2025-11-22 00:50:10 -0500

    Fix: Mejorar menú desplegable de servicios - delay y área de conexión

---
+++ _next/static/vzqRDLEBo29I3j2jXwDqN/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/educaplus//_next/:path+",
+        "destination": "/educaplus/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
